ddc6f97f8a14dfb6898c4b7036aafc56ca0e0e62
Nimna <e1846608@bit.mrt.ac.lk>
2022-04-11 08:23:26 +0530

    home template and profile template added

--- src/profiles/models.py
+++ src/profiles/models.py
@@ -1,9 +1,9 @@
-from distutils.command import upload
-from email.policy import default
 from django.db import models
+from django.shortcuts import reverse
 from django.contrib.auth.models import User
-from .utils import get_randomCode
-from django.template.defaultfilters import slugify
+from .utils import get_random_code
+from django.template.defaultfilters import default, slugify
+from django.db.models import Q
 
 # Create your models here.
 
@@ -29,20 +29,70 @@ class Profile(models.Model):
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
-    #string representation
-    def __str__(self):
-        return f"{self.user.username}-{self.created}"
+    def get_friends(self):
+        return self.friends.all()
 
-    #generate a random slug when there're 2 or more profiles with the same name
+    def get_friends_no(self):
+        return self.friends.all().count()
+
+    def __str__(self):
+        return f"{self.user.username}-{self.created.strftime('%d-%m-%y')}"
+
+    def get_absolute_url(self):
+        return reverse("profiles:profile-detail-view", kwargs={"slug": self.slug})
+        
+
+    
+    __initial_name = None
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__initial_name = self.first_name
+
     def save(self, *args, **kwargs):
-        sluExist = False
-        if self.first_name and self.last_name:
-            to_slug = slugify(str(self.first_name) + " " + str(self.last_name))
-            sluExist = Profile.objects.filter(slug=to_slug).exists()
-            while sluExist:
-                to_slug = slugify(to_slug + " " + str(get_randomCode()))
-                sluExist = Profile.objects.filter(slug=to_slug).exists()
-        else:
-            to_slug = str(self.user)
+        ex =False
+        to_slug = self.slug
+        if self.first_name != self.__initial_name or self.slug=="":
+            if self.first_name:
+                to_slug = slugify(str(self.first_name))
+                ex = Profile.objects.filter(slug = to_slug).exists()
+                while ex:
+                    to_slug = slugify(to_slug+" "+ str(get_random_code()))
+                    ex = Profile.objects.filter(slug = to_slug).exists()
+            else:
+                to_slug = str(self.user)
         self.slug = to_slug
         super().save(*args, **kwargs)
+
+
+STATUS_CHOICES = (
+    ('send','send'),
+    ('accepted','accepted')
+)
+
+
+class RelationshipManager(models.Manager):
+    def invitations_received(self, receiver):
+        qs = Relationship.objects.filter(receiver=receiver, status='send')
+        return qs 
+
+
+class Relationship(models.Model):
+    
+    #everytime a particular profile is deleted, the relationship will also be deleted. 
+    sender = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='sender')
+    receiver = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='receiver')
+
+    #to determine the status of the choice. whether it's a sent invitation or an invitation accepted
+    status = models.CharField(max_length=8, choices=STATUS_CHOICES)
+
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    objects = RelationshipManager()
+
+
+
+    def __str__(self):
+        return f"{self.sender}-{self.receiver}-{self.status}"
+             
