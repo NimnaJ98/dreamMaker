140df0f0ac8a4a09e7a584339df546ddc20ca713
HamziyaAJ2022 <103714461+HamziyaAJ2022@users.noreply.github.com>
2022-04-17 14:32:04 +0530

    Update admin.py

--- src/posts/admin.py
+++ src/posts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Profile, Relationship
+from .models import Post, Comment, Like
 # Register your models here.
 
-admin.site.register(profile)
-admin.site.register(Relationship)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Like)
