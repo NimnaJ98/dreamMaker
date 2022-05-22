2723fd7d2c8e6893af92aed4b737f4c6e8633e8b
Thushshan <thushshan96@gmail.com>
2022-05-22 13:45:29 +0530

    models added and registered

--- src/auditions/admin.py
+++ src/auditions/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Audition, EventComment, Interest
 # Register your models here.
+
+admin.site.register(Audition)
+admin.site.register(EventComment)
+admin.site.register(Interest)
--- src/auditions/models.py
+++ src/auditions/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.core.validators import FileExtensionValidator
+from profiles.models import Profile
+# Create your models here.
+
+class Audition(models.Model):
+    content = models.TextField()
+    image = models.ImageField(upload_to='auditions', validators=[FileExtensionValidator(['png', 'jpg', 'jpeg'])], blank=True)
+    interested = models.ManyToManyField(Profile, blank=True, related_name='interests')
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    organiser = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='auditions')
+
+    def __str__(self):
+        return str(self.content[:20])
+
+    def num_interests(self):
+        return self.interested.all().count()
+
+    def num_eventcomments(self):
+        return self.eventcomment_set.all().count()
+
+    class Meta:
+        ordering = ('-created',)
+
+class EventComment(models.Model):
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    audition = models.ForeignKey(Audition, on_delete=models.CASCADE)
+    body = models.TextField(max_length=300)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+            return str(self.pk)
+
+INTEREST_CHOICES = (
+    ('Interest', 'Interest'),
+    ('Uninterest', 'Uninterest'),
+)
+
+class Interest(models.Model): 
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    audition = models.ForeignKey(Audition, on_delete=models.CASCADE)
+    value = models.CharField(choices=INTEREST_CHOICES, max_length=10)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.user}-{self.audition}-{self.value}"
